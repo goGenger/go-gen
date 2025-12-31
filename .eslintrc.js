ee675d681aed0d3a65c7eaedccc372b8f5bde49d
shixianbao <2582266166@qq.com>
2025-12-31 14:52:10 +0800

    refactor: 移除 ESLint 配置中多余的 parser 配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,6 @@ module.exports = {
     es2021: true,
     jest: true,
   },
-  parser: '@typescript-eslint/parser',
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
